7ca689e5230ea49a6a65be2a09cc9fbf37c78f4a
tarentio62 <nicolas.greco.mail@gmail.com>
2013-05-22 15:25:48 +0300

    Update timers.js

--- timers.js
+++ timers.js
@@ -3,16 +3,21 @@ $(document).ready(function() {
   var inter = setInterval( function ()
   {
   var n = parseInt($("#lblNashor").text());
-  if (n > 1){
+	if (n > 1){
 	n--;
 	$("#lblNashor").text(n);
 	}
 	else
-	{	
+	{
+        navigator.notification.beep(3);
+		navigator.notification.vibrate(2000);
 	clearInterval(inter);
 	} 
 },1000);
 });
 });
+
+
+
 
 
